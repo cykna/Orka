5dc456206973d614c3003640760003b5f48cc7bd
caina <kina.lima1302@proton.me>
2026-05-01 19:56:50 -0300

    feat: made creation of process on new namespace

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,38 @@
 use clap::Parser;
-use nix::spawn::{PosixSpawnAttr, PosixSpawnFileActions};
-use std::ffi::CStr;
-use std::path::PathBuf;
+use nix::{
+    libc::{SIGCHLD, execve},
+    sched::CloneFlags,
+    sys::wait::waitpid,
+};
+use std::ptr::null;
 
 #[derive(Parser, Debug)]
 struct OrkaCli {
     #[arg(short, long)]
-    pub exec: PathBuf,
+    pub exec: String,
 }
-
+const STACK_SIZE: usize = 1024 * 1024; //1mb
 fn main() -> color_eyre::Result<()> {
     let cli = OrkaCli::parse();
-    let pid = nix::spawn::posix_spawn(
-        &cli.exec,
-        &PosixSpawnFileActions::init()?,
-        &PosixSpawnAttr::init()?,
-        &[] as &[&CStr],
-        &[] as &[&CStr],
-    )?;
-    println!("{:?}", pid);
+
+    let child = unsafe {
+        nix::sched::clone(
+            Box::new(move || {
+                let ptr = cli.exec.as_bytes().as_ptr() as *const i8;
+                let v = execve(ptr, null(), null());
+                println!("{v} Laburai");
+                0
+            }),
+            &mut [0; STACK_SIZE],
+            CloneFlags::CLONE_NEWPID | CloneFlags::CLONE_NEWNET,
+            Some(SIGCHLD),
+        )?
+    };
+
+    let v = waitpid(child, None)?;
+    println!("{v:?}");
+
+    println!("{child}");
 
     Ok(())
 }
